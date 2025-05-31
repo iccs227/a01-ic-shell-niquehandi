9cbe762c91abd998bdbda196f9732f0ed12d2fd3
Niqueeeee <103115381+niquehandi@users.noreply.github.com>
2025-05-31 22:08:53 +0700

    Update endin.h

---
+++ endin.h
@@ -0,0 +1,12 @@
+#ifndef ENDIN_H
+#define ENDIN_H
+
+
+
+// lists all the files in the current directory ending with the given suffix
+// suffix is to search for string with '.' like '.c' , '.h' ,'txt' etc
+// if no suffix is given, it will print usage message
+
+void cmd_endin(const char *sfx);
+#endif // ENDIN_H
+
